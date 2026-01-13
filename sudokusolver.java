d9f18378184c622054f6bd82071d1982cee151b9
Arsh <tiwariiiarsh1106@gmail.com>
2026-01-13 23:02:06 +0530

    add some beatiful comments and make some change in the code

--- sudokusolver.java
+++ sudokusolver.java
@@ -1,7 +1,10 @@
 package arsh.java.recursion;
 
 public class sudokusolver {
+
     public static void main(String[] args) {
+
+        // 0 represents empty cells in Sudoku
         int[][] board = new int[][]{
                 {3, 0, 6, 5, 0, 8, 4, 0, 0},
                 {5, 2, 0, 0, 0, 0, 0, 0, 0},
@@ -14,87 +17,104 @@ public class sudokusolver {
                 {0, 0, 5, 2, 0, 6, 3, 0, 0}
         };
 
+        // Try solving the Sudoku
         if (solve(board)) {
+            // If solved, print the board
             display(board);
         } else {
+            // If not solvable
             System.out.println("Cannot solve");
         }
-
     }
+
+    // Recursive function to solve Sudoku using backtracking
     static boolean solve(int[][] board) {
+
         int n = board.length;
+
+        // Variables to store row and column of an empty cell
         int row = -1;
         int col = -1;
 
-        boolean emptyLeft = true;
+        // Flag to check if any empty cell is left
+        boolean emptyLeft = false;
 
-        // this is how we are replacing the r,c from arguments
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 0) {
-                    row = i;
-                    col = j;
-                    emptyLeft = false;
-                    break;
+        // STEP 1: Find the first empty cell (value 0)
+        for (int i = 0; i < n; i++) {           // loop through rows
+            for (int j = 0; j < n; j++) {       // loop through columns
+                if (board[i][j] == 0) {         // empty cell found
+                    row = i;                    // store row index
+                    col = j;                    // store column index
+                    emptyLeft = true;           // mark empty found
+                    break;                      // break inner loop
                 }
             }
-            // if you found some empty element in row, then break
-            if (emptyLeft == false) {
+            // If empty cell found, break outer loop too
+            if (emptyLeft) {
                 break;
             }
         }
 
-        if (emptyLeft == true) {
+        // STEP 2: If no empty cell is left, Sudoku is solved
+        if (!emptyLeft) {
             return true;
-            // soduko is solved
         }
 
-        // backtrack
+        // STEP 3: Try numbers 1 to 9 in the empty cell
         for (int number = 1; number <= 9; number++) {
+
+            // Check if placing this number is safe
             if (isSafe(board, row, col, number)) {
+
+                // Place the number in the empty cell
                 board[row][col] = number;
+
+                // STEP 4: Recursively solve the remaining board
                 if (solve(board)) {
-                    // found the answer
+                    // If recursive call returns true, solution is found
                     return true;
                 } else {
-                    // backtrack
+                    // STEP 5: Backtrack (undo the change)
                     board[row][col] = 0;
                 }
             }
         }
+
+        // If no number fits, return false (dead end)
         return false;
     }
 
+    // Function to print the Sudoku board
     private static void display(int[][] board) {
-        for(int[] row : board) {
-            for(int num : row) {
+        for (int[] row : board) {           // loop through rows
+            for (int num : row) {           // loop through columns
                 System.out.print(num + " ");
             }
             System.out.println();
         }
     }
 
-
+    // Function to check whether placing a number is safe
     static boolean isSafe(int[][] board, int row, int col, int num) {
-        // check the row
+
+        // Check if number already exists in the same row
         for (int i = 0; i < board.length; i++) {
-            // check if the number is in the row
             if (board[row][i] == num) {
                 return false;
             }
         }
 
-        // check the col
+        // Check if number already exists in the same column
         for (int[] nums : board) {
-            // check if the number is in the col
             if (nums[col] == num) {
                 return false;
             }
         }
 
-        int sqrt = (int) (Math.sqrt(board.length));
-        int rowStart = row - row % sqrt;
-        int colStart = col - col % sqrt;
+        // Check in the 3x3 subgrid
+        int sqrt = (int) Math.sqrt(board.length); // sqrt(9) = 3
+        int rowStart = row - row % sqrt;          // top-left row of subgrid
+        int colStart = col - col % sqrt;          // top-left column of subgrid
 
         for (int r = rowStart; r < rowStart + sqrt; r++) {
             for (int c = colStart; c < colStart + sqrt; c++) {
@@ -103,7 +123,8 @@ public class sudokusolver {
                 }
             }
         }
+
+        // Safe to place number
         return true;
     }
-
 }
